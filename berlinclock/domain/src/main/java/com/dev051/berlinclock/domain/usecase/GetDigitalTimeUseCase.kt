88fb6db5f89f486fa63bda116a4acbc20e187bfe
Oiodev <cedricgoffoy@gmail.com>
2026-06-21 14:03:12 +0200

    pass fourth digital time test

--- berlinclock/domain/src/main/java/com/dev051/berlinclock/domain/usecase/GetDigitalTimeUseCase.kt
+++ berlinclock/domain/src/main/java/com/dev051/berlinclock/domain/usecase/GetDigitalTimeUseCase.kt
@@ -9,12 +9,17 @@ class GetDigitalTimeUseCase {
     operator fun invoke(state: BerlinClockState): LocalTime {
         val hourBlocks = state.hourBlocks.filter { it != LightState.OFF }.size
         val hours = state.hours.filter { it != LightState.OFF }.size
+
+        val minuteBlocks = state.minuteBlocks.filter { it != LightState.OFF }.size
+        val minutes = state.minutes.filter { it != LightState.OFF }.size
+
         // seconds are lost with the Berlin clock system.
         // As digital time also pulses every even second, we can fake it with even or odd value second
         // so the display will shift between (HH : MM) and (HH  MM)
         val seconds = if (state.isSecondEven) 0 else 1
         return LocalTime.MIDNIGHT
             .withHour((hourBlocks * 5) + hours)
+            .withMinute((minuteBlocks * 5) + minuteBlocks)
             .withSecond(seconds)
     }
 }
