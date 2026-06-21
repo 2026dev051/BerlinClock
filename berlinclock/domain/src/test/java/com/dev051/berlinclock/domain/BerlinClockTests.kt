55db22e3fd969c045e3328683f7cb5eb48f5ecb9
Oiodev <cedricgoffoy@gmail.com>
2026-06-21 13:03:53 +0200

    create test with random value to validate algorithms

--- berlinclock/domain/src/test/java/com/dev051/berlinclock/domain/BerlinClockTests.kt
+++ berlinclock/domain/src/test/java/com/dev051/berlinclock/domain/BerlinClockTests.kt
@@ -57,7 +57,7 @@ class BerlinClockTests {
     }
 
     @Test
-    fun `at 1AM, only the first hour is lit RED and seconds light is lit`() {
+    fun `at 01h00, only the hours is lit ROOO and seconds light is lit`() {
         val time = LocalTime.of(1, 0, 0)
         val state = getBerlinClock(time)
 
@@ -101,7 +101,7 @@ class BerlinClockTests {
     }
 
     @Test
-    fun `at 5AM, only the first hour block is lit RED and seconds light is lit`() {
+    fun `at 05h00, only the hourBlocks is lit ROOO and seconds light is lit`() {
         val time = LocalTime.of(5, 0, 0)
         val state = getBerlinClock(time)
 
@@ -145,7 +145,7 @@ class BerlinClockTests {
     }
 
     @Test
-    fun `at 6h01 AM, the first hour block is lit RED, the first hour is lit RED, the first minute is lit RED and seconds light is lit`() {
+    fun `at 06h01, the hourBlocks is lit ROOO, the hours is lit ROOO, the minutes is lit ROOO and seconds light is lit`() {
         val time = LocalTime.of(6, 1, 0)
         val state = getBerlinClock(time)
 
@@ -189,7 +189,7 @@ class BerlinClockTests {
     }
 
     @Test
-    fun `at 11h23 AM, the first two hour blocks are lit RED, the first hour is lit RED, the four first minute blocks are lit RRYR, the first three minutes are lit RED and seconds light is lit`() {
+    fun `at 11h23, the hourBlocks is lit RROO, the hours is lit ROOO, the minuteBlocks is lit YYRYOOOOOOO, the minutes is lit RRRO and seconds light is lit`() {
         val time = LocalTime.of(11, 23, 0)
         val state = getBerlinClock(time)
 
@@ -227,6 +227,50 @@ class BerlinClockTests {
                     LightState.OFF,
                 ),
                 isSecondEven = true
+            ),
+            state,
+        )
+    }
+
+    @Test
+    fun `at 17h37 23s, the hourBlocks is lit RRRO, the hours is lit RROO, the minuteBlocks is lit YYRYYRYOOOO, the minutes is lit RROO and seconds light is off`() {
+        val time = LocalTime.of(17, 37, 23)
+        val state = getBerlinClock(time)
+
+        assertEquals(
+            BerlinClockState(
+                hourBlocks = listOf(
+                    LightState.RED,
+                    LightState.RED,
+                    LightState.RED,
+                    LightState.OFF,
+                ),
+                hours = listOf(
+                    LightState.RED,
+                    LightState.RED,
+                    LightState.OFF,
+                    LightState.OFF,
+                ),
+                minuteBlocks = listOf(
+                    LightState.YELLOW,
+                    LightState.YELLOW,
+                    LightState.RED,
+                    LightState.YELLOW,
+                    LightState.YELLOW,
+                    LightState.RED,
+                    LightState.YELLOW,
+                    LightState.OFF,
+                    LightState.OFF,
+                    LightState.OFF,
+                    LightState.OFF,
+                ),
+                minutes = listOf(
+                    LightState.RED,
+                    LightState.RED,
+                    LightState.OFF,
+                    LightState.OFF,
+                ),
+                isSecondEven = false
             ),
             state,
         )
