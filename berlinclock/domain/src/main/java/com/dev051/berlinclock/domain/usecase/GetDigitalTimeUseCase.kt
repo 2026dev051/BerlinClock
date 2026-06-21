e8634729adb726b5d54f500ef0d525c4254cba73
Oiodev <cedricgoffoy@gmail.com>
2026-06-21 13:32:17 +0200

    pass first digital time test

--- berlinclock/domain/src/main/java/com/dev051/berlinclock/domain/usecase/GetDigitalTimeUseCase.kt
+++ berlinclock/domain/src/main/java/com/dev051/berlinclock/domain/usecase/GetDigitalTimeUseCase.kt
@@ -6,6 +6,10 @@ import java.time.LocalTime
 class GetDigitalTimeUseCase {
 
     operator fun invoke(state: BerlinClockState): LocalTime {
-        return LocalTime.MIDNIGHT
+        // seconds are lost with the Berlin clock system.
+        // As digital time also pulses every even second, we can fake it with even or odd value second
+        // so the display will shift between (HH : MM) and (HH  MM)
+        val seconds = if (state.isSecondEven) 0 else 1
+        return LocalTime.MIDNIGHT.withSecond(seconds)
     }
 }
