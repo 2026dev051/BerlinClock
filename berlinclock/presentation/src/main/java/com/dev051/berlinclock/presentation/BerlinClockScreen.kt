89d2ce4cdc74625eac53c8409e990b950c683f97
Oiodev <cedricgoffoy@gmail.com>
2026-06-23 19:29:57 +0200

    add styling to finalize the design

--- berlinclock/presentation/src/main/java/com/dev051/berlinclock/presentation/BerlinClockScreen.kt
+++ berlinclock/presentation/src/main/java/com/dev051/berlinclock/presentation/BerlinClockScreen.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
@@ -18,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dev051.berlinclock.domain.model.BerlinClockState
@@ -29,8 +32,9 @@ fun BerlinClock(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier.padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         val hours = remember { derivedStateOf { state.hours } }
         val hourBlocks = remember { derivedStateOf { state.hourBlocks } }
@@ -68,18 +72,33 @@ private fun HorizontalLights(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        lightState.value.forEach { light ->
+        lightState.value.forEachIndexed { index, light ->
+            val shape = when (index) {
+                0 -> RoundedCornerShape(24.dp, 0.dp, 0.dp, 24.dp)
+                lightState.value.lastIndex -> RoundedCornerShape(0.dp,24.dp, 24.dp, 0.dp)
+                else -> RectangleShape
+            }
             Box(
                 modifier = Modifier
                     .weight(1F)
                     .height(48.dp)
-                    .border(2.dp, Color.DarkGray)
+                    .clip(shape)
+                    .background(light.toColor())
+                    .border(2.dp, Color.DarkGray, shape)
             )
         }
     }
 }
 
-@Preview
+private fun LightState.toColor(): Color {
+    return when (this) {
+        LightState.RED -> Color.Red
+        LightState.YELLOW -> Color.Yellow
+        LightState.OFF -> Color.LightGray
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 private fun BerlinClockPreview() {
     BerlinClock(
